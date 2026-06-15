f889f25bffa5cd28154386bb81f3e84c52fa8dfb
RaniFerreira <franielly535@gmail.com>
2026-06-15 14:24:44 -0300

    adicionando o controller do usuario

---
+++ src/main/java/ener/controller/UserController.java
@@ -0,0 +1,21 @@
+package ener.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// Controller responsável pelas páginas de login e acesso negado
+@Controller
+public class UserController {
+
+    // Exibe a página de login
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    // Exibe a página de acesso negado
+    @GetMapping("/accessDenied")
+    public String getAccessDeniedPage() {
+        return "accessDenied";
+    }
+}
